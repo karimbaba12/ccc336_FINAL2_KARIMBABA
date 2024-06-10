e7edb6ad017ec0bfe66b4f2058eb8e8771d5a668
karimbaba12 <babakarim123456@gmail.com>
2024-06-10 10:05:11 +0300

    Adding Mapping between class and DTO

---
+++ FINAL_BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FINAL_BLL.DTO;
+using FINAL_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINAL_BLL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+        CreateMap<Airplane,AirplaneDTO>().ReverseMap();
+            CreateMap<Flight,FlightDTO>().ReverseMap();
+            CreateMap<Passenger,PassengerDTO>().ReverseMap();
+            CreateMap<Reservation,ReservationDTO>().ReverseMap();
+            CreateMap<Pilot,PilotDTO>().ReverseMap();
+        }
+    }
+}
